0e226ef75f2e3414c789ea595d34dc906277de17
PresentKim <debe3721@gmail.com>
2020-11-29 23:49:28 +0900

    Add '404 not found' handler

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const app = express();
 
 app.use(express.json());
 app.use(userRouter);
+app.use((req, res) => {
+    res.status(404).send({message: `Not found method: ${req.path}`});
+});
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
