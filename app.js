2bb54f264ed8b9a0992fc69b267d2585a028c85e
PresentKim <debe3721@gmail.com>
2020-11-29 23:43:34 +0900

    Remove node-sass-middleware

--- app.js
+++ app.js
@@ -1,18 +1,10 @@
 const express = require('express');
-const path = require('path');
-const sassMiddleware = require('node-sass-middleware');
 const userRouter = require('./routers/user');
 const port = process.env.PORT;
 
 const app = express();
 
 app.use(express.json());
-app.use(sassMiddleware({
-    src: path.join(__dirname, 'public'),
-    dest: path.join(__dirname, 'public'),
-    indentedSyntax: true, // true = .sass and false = .scss
-    sourceMap: true
-}));
 app.use(userRouter);
 
 app.listen(port, () => {
